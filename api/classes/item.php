54e216eeb76778e9d9a49c2966dac3f8ae0150d7
Matt Phillips <mphillips@law.harvard.edu>
2012-05-03 13:12:37 -0400

    reworked our data model. Now storing item related info only once, but keeping a counter (for times awesomed) and last-modified field. the checked-in business went to its own Elasticsearch index

--- api/classes/item.php
+++ api/classes/item.php
@@ -38,7 +38,7 @@ class Item extends F3instance {
                 title: {
                     order: "desc"
                 }
-            }s
+            }
         }';
         */
         $request = array();
@@ -97,61 +97,22 @@ class Item extends F3instance {
         echo $this->render($path_to_template);
     }
     
-    function most_awesome() {
-        // Get the most awesomed (the items that have been awesomed 
-        // the greatest number of times)
-        
-        /* Start building our query with facet object. We want to end up here:
-        {
-            "query" : {
-                "match_all" : {  }
-            },
-            "size" : 0,
-            "facets" : {
-                "tag" : {
-                    "terms" : {
-                        "field" : "hollis_id",
-                        "size" : 100
-                    }
-                }
-            }
-        }
-        */
+    function recently_awesome() {
+        // Get the recently awesome (the items that were most recently
+        // dropped in the Awesome Box)
         
         $request = array();
+
+        // Match all items
         $request['query'] = array("match_all" => new stdClass);
-    
-        $request['size'] = 0;
-    
-        $facets = array('tag' => array('terms' => array('field' => 'hollis_id')));
-
-        $request['facets'] = $facets;
-    
-        $jsoned_request = json_encode($request);
         
-        $url = $this->get('ELASTICSEARCH_URL') . '_search';
-        $ch = curl_init();
-        $method = "GET";
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoned_request);
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $results = json_decode($response, True);
-
-        // At this point we have a list of most awesome items. Let's hit ES again for details on that list
-        $most_aweseome_hollis_ids = array();
-        foreach ($results['facets']['tag']['terms'] as $facet) {
-            $most_aweseomed_hollis_ids[] =  $facet['term'];
-        }
-        
-        $request = array();
-        $request['query']['terms']['hollis_id'] = $most_aweseomed_hollis_ids;
+        // limit parameter (elasticsearch calls this 'size')
         $request['size'] = 9;
         
+        // sort parameter
+        $request['sort'] = array('last_modified' => array('order' => 'desc'));
+        
+        // We now have our built request, let's jsonify it and send it to ES
         $jsoned_request = json_encode($request);
         
         $url = $this->get('ELASTICSEARCH_URL') . '_search';
@@ -167,20 +128,58 @@ class Item extends F3instance {
         curl_close($ch);
         
         // We should have a response. Let's pull the docs out of it
-        $extracted_results = $this->get_docs_from_es_response(json_decode($results, True));
+        $cleaned_results = $this->get_docs_from_es_response(json_decode($results, True));
         
-        $this->set('results', $extracted_results);
+        $this->set('results', $cleaned_results);
+        $path_to_template = 'api/templates/search_json.php';
+        echo $this->render($path_to_template);
+
+    }
+    
+    function most_awesome() {
+        // Get the most awesomed (the items that have been awesomed 
+        // the greatest number of times)
+        
+        $request = array();
+
+        // Match all items
+        $request['query'] = array("match_all" => new stdClass);
+        
+        // limit parameter (elasticsearch calls this 'size')
+        $request['size'] = 9;
+        
+        // sort parameter
+        $request['sort'] = array('awesomed' => array('order' => 'desc'));
+        
+        // We now have our built request, let's jsonify it and send it to ES
+        $jsoned_request = json_encode($request);
+        
+        $url = $this->get('ELASTICSEARCH_URL') . '_search';
+        $ch = curl_init();
+        $method = "GET";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoned_request);
+
+        $results = curl_exec($ch);
+        curl_close($ch);
+        
+        // We should have a response. Let's pull the docs out of it
+        $cleaned_results = $this->get_docs_from_es_response(json_decode($results, True));
+        
+        $this->set('results', $cleaned_results);
         $path_to_template = 'api/templates/search_json.php';
         echo $this->render($path_to_template);
 
     }
 
     function create() {
-        // create an item
-        // TODO: We're doing no validation here. this is bad.
+        // An item has been Awesomed (we received a barcode)
         
+        // Start buliding the item
         $new_item = array();
-        
         $incoming_title = $this->get('POST.title');
         if(!empty($incoming_title)) {
              $new_item['title'] = $incoming_title;
@@ -203,13 +202,51 @@ class Item extends F3instance {
              $new_item['library'] = $incoming_library;
         }
         
-        $new_item['id'] = $this->gen_uuid();
-        
-        $new_item['checked_in'] = time();
+        // This helps us get the recently awesome
+        $now = time();
+        $new_item['last_modified'] = $now;
 
-        $jsoned_new_item = json_encode($new_item);
+        // We now have now built the item. Ask ES if it has it.
+        $request = array();
+        $request['query']['terms'] = array('hollis_id' => array($incoming_hollis_id));
+                
+        $jsoned_request = json_encode($request);
         
+        $url = $this->get('ELASTICSEARCH_URL') . '_search';
+        $ch = curl_init();
+        $method = "GET";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoned_request);
+
+        $results = curl_exec($ch);
+        curl_close($ch);
+        
+        $docs = json_decode($results, True);
+
+
         $url = $this->get('ELASTICSEARCH_URL');
+
+        // See if we already have it. If we do, just bump its awesomed (the counter field)
+        if ($docs['hits']['total'] > 0) {
+            
+            $current_count = $docs['hits']['hits'][0]['_source']['awesomed'];
+            $current_count = $current_count + 1;
+            $new_item['awesomed'] = $current_count;
+            
+            $url = $url . '/' . $docs['hits']['hits'][0]['_id'];
+            
+        } else {
+            // It's not in ES. We need to add it.
+
+            $new_item['awesomed'] = 1;
+            
+        }
+        
+        // Send the item back to ES.
+        $jsoned_new_item = json_encode($new_item);
         $ch = curl_init();
         $method = "POST";
 
@@ -217,6 +254,26 @@ class Item extends F3instance {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoned_new_item);
+
+        $results = curl_exec($ch);
+        curl_close($ch);
+        
+        
+        // We should now have the item details stored. Let's index the Awesomed event (hollis_id and timestamp)
+        $awesomed_container = array();
+        $awesomed_container['hollis_id'] = $incoming_hollis_id;
+        $awesomed_container['checked_in'] = $now;
+    
+        // Send the event to ES.
+        $url = $this->get('ELASTICSEARCH_URL_CHECKED_IN');
+        $jsoned_awesomed_container = json_encode($awesomed_container);
+        $ch = curl_init();
+        $method = "POST";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoned_awesomed_container);
 
         $results = curl_exec($ch);
         curl_close($ch);
@@ -344,6 +401,12 @@ class Item extends F3instance {
                 }
                 if (!empty($result['_source']['id'])) {
                     $doc['id'] = $result['_source']['id'];
+                }
+                if (!empty($result['_source']['awesomed'])) {
+                    $doc['awesomed'] = $result['_source']['awesomed'];
+                }
+                if (!empty($result['_source']['last_modified'])) {
+                    $doc['last_modified'] = $result['_source']['last_modified'];
                 }
                 array_push($docs, $doc);
             }
