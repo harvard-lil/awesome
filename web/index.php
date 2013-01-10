5043d9d36b1b3c445c9ae67be3db322b8b7bd3f1
Matt Phillips <mphillips@law.harvard.edu>
2013-01-10 11:11:54 -0500

    fixes #10

--- web/index.php
+++ web/index.php
@@ -186,7 +186,21 @@
           </li>
         {{/docs}}
       </ul>
-  </script> 
+  </script>
+  
+    <script type="text/javascript">
+
+        var _gaq  = _gaq || [];
+        _gaq.push(['_setAccount', '<?php echo $master_config["GOOGLE_ANALYTICS"];?>']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
 	
 	</body>
   
