bddbb0024c8cd5f7f3752d41c131c1176b79e1cc
Annie Cain <anniejocain@gmail.com>
2013-02-14 15:37:02 -0500

    fixed url paths for widget

--- lil/awesome/templates/widget.js
+++ lil/awesome/templates/widget.js
@@ -4,14 +4,14 @@
 var fileref=document.createElement("link");
 fileref.setAttribute("rel", "stylesheet");
 fileref.setAttribute("type", "text/css");
-fileref.setAttribute("href", "{{organization.slug}}.{{awesome_domain}}{{ STATIC_PREFIX }}css/widget.css");
+fileref.setAttribute("href", "http://{{organization.slug}}.{{awesome_domain}}{{ STATIC_PREFIX }}css/widget.css");
 //fileref.setAttribute("href", "{{ STATIC_PREFIX }}css/widget.css");
 document.getElementsByTagName("head")[0].appendChild(fileref);
 
 document.write('<div class="aw-widget">');
-document.write('<img src="{{organization.slug}}.{{awesome_domain}}{{ STATIC_PREFIX }}images/widget-arrow.png" class="aw-widget-arrow" />');
+document.write('<img src="http://{{organization.slug}}.{{awesome_domain}}{{ STATIC_PREFIX }}images/widget-arrow.png" class="aw-widget-arrow" />');
 //document.write('<img src="{{ STATIC_PREFIX }}images/widget-arrow.png" class="aw-widget-arrow" />');
-document.write('<h4 class="aw-widget-title"><a href="{{organization.slug}}.{{awesome_domain}}">Awesome at {{organization}}</a></h4>');
+document.write('<h4 class="aw-widget-title"><a href="http://{{organization.slug}}.{{awesome_domain}}">Awesome at {{organization}}</a></h4>');
 document.write('<div class="aw-widget-list">');
 {% for item in items %}		
 	document.write('<p class="aw-widget-item"><a href="{{organization.catalog_base_url}}{{item.catalog_id}}" target="_blank">{{item.title}}</a><br><span class="aw-widget-creator">{{item.creator}}</span></p>');
