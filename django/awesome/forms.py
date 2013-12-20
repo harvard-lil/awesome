135f8737a459b7dee84452d58a15b5b8f409be6e
Annie Cain <anniejocain@gmail.com>
2013-12-20 09:55:38 -0500

    closes #76

--- django/awesome/forms.py
+++ django/awesome/forms.py
@@ -68,7 +68,7 @@ class OrganizationForm(forms.ModelForm):
 
     class Meta:
         model = Organization
-        exclude = ('user', 'slug', 'service_lookup', 'twitter_username', 'twitter_oauth_token', 'twitter_oauth_secret',)
+        exclude = ('user', 'slug', 'service_lookup', 'twitter_username', 'twitter_oauth_token', 'twitter_oauth_secret', 'twitter_consumer_key', 'twitter_consumer_secret',)
         
 class OrganizationFormRegistration(forms.ModelForm):
             
