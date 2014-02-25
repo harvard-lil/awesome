daaa1ae4bb69dd7bce4b64cccfbed72736dd6846
Annie Cain <anniejocain@gmail.com>
2014-02-25 14:54:08 -0500

    added top items and top authors to landing page

--- django/awesome/views/landing.py
+++ django/awesome/views/landing.py
@@ -1,9 +1,10 @@
 import logging
 
-from awesome.models import Organization
+from awesome.models import Organization, Item
 
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from django.db.models import Count, Sum
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +29,10 @@ def landing(request):
                                                'branch': branch, 'twitter_username': org.twitter_username,
                                                'ga_key': GOOGLE['ANALYTICS_KEY']})
     else:
-        context = {'ga_key': GOOGLE['ANALYTICS_KEY']}
+        items = Item.objects.values('title').annotate(total_checkins=Sum('number_checkins')).order_by('-total_checkins')[:5]
+        creators = Item.objects.values('creator').annotate(total_checkins=Sum('number_checkins')).order_by('-total_checkins')[:5]
+        logger.debug(items)
+        context = {'ga_key': GOOGLE['ANALYTICS_KEY'], 'items': items, 'creators': creators}
                
         context.update(csrf(request))
         return render_to_response('landing_default.html', context)
